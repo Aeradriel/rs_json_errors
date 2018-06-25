0a6caf78f175cecc4476d0fc2abacb93b78471ba
Thibaut Roche <thibaut.roche.perso@gmail.com>
2018-06-25 17:52:13 +0200

    [C] Refactor + NotFound error handling

--- src/json_errors.rs
+++ src/json_errors.rs
@@ -1,4 +1,6 @@
-use diesel::result::{DatabaseErrorInformation, DatabaseErrorKind, Error::DatabaseError};
+use diesel::result::{
+    DatabaseErrorInformation, DatabaseErrorKind, Error::DatabaseError, Error::NotFound,
+};
 use regex::Regex;
 use rocket::http::ContentType;
 use rocket::http::Status;
@@ -10,101 +12,114 @@ use rocket_contrib::Json;
 pub struct JsonErrors(pub Vec<JsonError>, pub Status);
 
 impl<'r> Responder<'r> for JsonErrors {
-  fn respond_to(self, req: &Request) -> response::Result<'r> {
-    let errors = self.0;
-    let vec_errors = errors
-      .into_iter()
-      .map(|error| error.description)
-      .collect::<Vec<String>>();
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        let errors = self.0;
+        let vec_errors = errors
+            .into_iter()
+            .map(|error| error.description)
+            .collect::<Vec<String>>();
 
-    let errors_description = vec_errors.join("\n");
+        let errors_description = vec_errors.join("\n");
 
-    let body = Json(json!({
-      "error": errors_description,
-      "errors": vec_errors 
-    }));
+        let body = Json(json!({
+            "error": errors_description,
+            "errors": vec_errors
+        }));
 
-    let mut res = body.respond_to(req).unwrap();
-    res.set_status(self.1);
-    res.set_header(ContentType::JSON);
-    Ok(res)
-  }
+        let mut res = body.respond_to(req).unwrap();
+        res.set_status(self.1);
+        res.set_header(ContentType::JSON);
+        Ok(res)
+    }
 }
 
 #[derive(Debug)]
 pub struct JsonError {
-  status: Status,
-  description: String,
-  body: Json,
+    status: Status,
+    description: String,
+    body: Json,
 }
 
 impl JsonError {
-  pub fn new(status: Status, description: &str) -> Self {
-    JsonError {
-      status,
-      description: String::from(description),
-      body: Json(json!({ "error": description.to_string() })),
+    pub fn new(status: Status, description: &str) -> Self {
+        JsonError {
+            status,
+            description: String::from(description),
+            body: Json(json!({ "error": description.to_string() })),
+        }
     }
-  }
+}
+
+impl<'b> From<JsonError> for JsonErrors {
+    fn from(err: JsonError) -> JsonErrors {
+        let status = err.status.clone();
+
+        JsonErrors(vec![err], status)
+    }
 }
 
 impl<'b> From<&'b diesel::result::Error> for JsonError {
-  fn from(err: &'b diesel::result::Error) -> JsonError {
-    let mut origin = String::from("An error occured");
-    let mut cause = "";
-    let mut status = Status::InternalServerError;
+    fn from(err: &'b diesel::result::Error) -> JsonError {
+        let ocs = match err {
+            DatabaseError(kind, infos) => match kind {
+                DatabaseErrorKind::UniqueViolation => (
+                    column_from_database_error_infos(infos),
+                    String::from("already exists"),
+                    Status::UnprocessableEntity,
+                ),
+                DatabaseErrorKind::ForeignKeyViolation => (
+                    column_from_database_error_infos(infos),
+                    String::from("violates foreign key"),
+                    Status::UnprocessableEntity,
+                ),
+                _ => (
+                    None,
+                    String::from("during a database transaction"),
+                    Status::InternalServerError,
+                ),
+            },
+            NotFound => (None, String::from("Not found"), Status::NotFound),
+            err => (
+                Some(String::from("Database error")),
+                format!("{:?}", err),
+                Status::InternalServerError,
+            ),
+        };
 
-    if let DatabaseError(kind, infos) = err {
-      match kind {
-        DatabaseErrorKind::UniqueViolation => {
-          cause = "already exists";
-          status = Status::UnprocessableEntity;
-          if let Some(name) = column_from_database_error_infos(infos) {
-            origin = name;
-          }
+        let mut error = vec![];
+
+        if let Some(origin) = ocs.0 {
+            error.push(origin);
         }
-        DatabaseErrorKind::ForeignKeyViolation => {
-          cause = "violates foreign key";
-          status = Status::UnprocessableEntity;
-          if let Some(name) = column_from_database_error_infos(infos) {
-            origin = name;
-          }
-        }
-        _ => cause = "during a database transaction",
-      }
+        error.push(ocs.1);
+
+        JsonError::new(ocs.2, &error.join(" "))
     }
-    if cause.is_empty() {
-      JsonError::new(status, &origin)
-    } else {
-      JsonError::new(status, &format!("{} {}", origin, cause))
-    }
-  }
 }
 
 impl<'r> Responder<'r> for JsonError {
-  fn respond_to(self, req: &Request) -> response::Result<'r> {
-    let mut res = self.body.respond_to(req).unwrap();
-    res.set_status(self.status);
-    res.set_header(ContentType::JSON);
-    Ok(res)
-  }
+    fn respond_to(self, req: &Request) -> response::Result<'r> {
+        let mut res = self.body.respond_to(req).unwrap();
+        res.set_status(self.status);
+        res.set_header(ContentType::JSON);
+        Ok(res)
+    }
 }
 
 fn column_from_database_error_infos(
-  infos: &Box<DatabaseErrorInformation + Send + Sync>,
+    infos: &Box<DatabaseErrorInformation + Send + Sync>,
 ) -> Option<String> {
-  if let Some(column) = infos.column_name() {
-    Some(String::from(column))
-  } else if let Some(constraint) = infos.constraint_name() {
-    let mut origin = String::from(constraint);
+    if let Some(column) = infos.column_name() {
+        Some(String::from(column))
+    } else if let Some(constraint) = infos.constraint_name() {
+        let mut origin = String::from(constraint);
 
-    if let Some(table_name) = infos.table_name() {
-      if let Ok(regex) = Regex::new(&format!(r"{}_(.+)_key", table_name)) {
-        origin = String::from(regex.replace(constraint, "$1"));
-      }
+        if let Some(table_name) = infos.table_name() {
+            if let Ok(regex) = Regex::new(&format!(r"{}_(.+)_key", table_name)) {
+                origin = String::from(regex.replace(constraint, "$1"));
+            }
+        }
+        Some(origin)
     }
-    Some(origin)
-  } else {
     None
-  }
 }
